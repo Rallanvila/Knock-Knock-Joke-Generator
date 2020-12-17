3d97553d2f50003fb133460ed6e3153a6bb22484
Allan <74523880+Rallanvila@users.noreply.github.com>
2020-12-17 01:43:34 -0500

    -m "added random homeOwner responses"

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,5 +1,5 @@
 const knockKnock = "Knock Knock!!";
-const homeOwner = "Who's There!?????";
+const homeOwner = ["Who's There!?????", "Well who's there now?", "Hello...Well, I said Hello! Who's There??", "Hangon, I'm getting dressed...umm Who's there?", "I have a gun!!! Who is it!!!??", "Who in the Sam Hell is at my door!!!!"];
 const knocker = ['Owls say', 'Cabbage', 'Ida', 'A litle old lady', 'To', 'Spell', 'Smellmop', 'I am', 'Cash', 'Amish', 'Ya', 'Billy Bob Joe Penny', 'FBI.', 'Cow says', 'Says', 'Tank', 'Hike', 'Hatch','Europe' ]
 //const response = `${knockGen(knocker)} whoo!!?????`
 const joke = ['Yes. Yes, they do', 'You expect a cabbage to have a last name?', 'Surely, it’s pronounced Idaho?', 'Wow, I didn’t know you could yodel!', 'No, it’s to whom.', 'W-H-O.', 'Gross! No thanks!', 'Are you having an identity crisis?', 'No thanks, but I’d love some peanuts', 'Really, you’re a shoe? Uh, okay.', 'No thanks, I use Google.', 'Really? How many Billy Bob Joe Pennies do you know?', 'We\'re asking the questions here!', 'No, cow says, Mooooooo!', 'Says me, that’s who!', 'You’re welcome.', 'I didn’t know you liked Japanese poetry!', 'God bless you!', 'No, you’re a poo!', ]
@@ -7,23 +7,23 @@ const joke = ['Yes. Yes, they do', 'You expect a cabbage to have a last name?', 
 const randomGen = () => {
     let random = Math.floor(Math.random() * 19);
     let knockResp = [knocker[random]];
+    let homeOwnerAnswer = homeOwner[Math.floor(Math.random() * 6)]
     let jokeResp = [joke[random]];
     //return knockResp + ' ' + knockResp + ' who!!???' + ' ' + jokeResp
-    return `${knockKnock} ${homeOwner} ${knockResp}. ${knockResp} who!!??? ${jokeResp}`
+    return `${knockKnock} ${homeOwnerAnswer} ${knockResp}. ${knockResp} who!!??? ${jokeResp}`
 }
 console.log(randomGen())
 
-const knockGen = arr => {
-    knockerResponse = []
-    knockerResponse.push(arr[Math.floor(Math.random() * 19)])
-    return knockerResponse
-    } 
+// const knockGen = arr => {
+//     knockerResponse = []
+//     knockerResponse.push(arr[Math.floor(Math.random() * 19)])
+//     return knockerResponse
+//     } 
 
-const jokeGen = arr => {
-    jokeResponse = [];
-    jokeResponse.push(arr[Math.floor(Math.random() * 19)])
-    return jokeResponse
-}
+// const jokeGen = arr => {
+//     jokeResponse = [];
+//     jokeResponse.push(arr[Math.floor(Math.random() * 19)])
+//     return jokeResponse
 //console.log(knockKnock + ' ' + homeOwner + ' ' + knockGen(knocker) + ' ' + ' ' + jokeGen(joke));
 
 
